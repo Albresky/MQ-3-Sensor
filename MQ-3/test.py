41a818d9cea74858e53fbf68f2ddb78dcecd8788
Albresky <78648318+Albresky@users.noreply.github.com>
2022-03-02 12:03:29 +0800

    update

--- MQ-3/test.py
+++ MQ-3/test.py
@@ -1,17 +1,7 @@
-import smbus
-import time
+import MQ3
 
-address = 0x48
-A0 = 0x40
-A1 = 0x41
-A2 = 0x42
-A3 = 0x43
-
-bus = smbus.SMBus(1)
+Gas = mygas
 
 while True:
-    bus.write_byte(address,A0)
-    value=bus.read_byte(address)              
-    print((value)/105*4.216)
-#     print(value)
-    time.sleep(1)
+    print(mygas.getGas())
+    time.sleep(0.5)
